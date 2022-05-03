95a9016947cf0b5a427f16e1811f9c004e3a1dd6
SaeGot <saegot@naver.com>
2022-05-03 18:57:44 +0900

    NeuralNetwork 준비중

---
+++ ColaLearningUnity/Tensor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColaLearningUnity
+{
+    internal class Tensor
+    {
+        private int x;
+        private int y;
+        private bool bias;
+    }
+
+    internal class TensorConnection
+    {
+        private Tensor previousTensor;
+        private Tensor nextTensor;
+    }
+}
